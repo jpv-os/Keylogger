67c4f44d6af5c1b098ffe6faf8d8528cdcb039c9
jpv-os <jp.vogtherr@gmail.com>
2024-02-15 00:16:55 +0100

    Install sqlite

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,10 @@ repositories {
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
-//    implementation(libs.annotations)
+    implementation(libs.annotations)
+    implementation("org.xerial:sqlite-jdbc:3.45.1.0") {
+        exclude("org.slf4j", "slf4j-api") // causes error if not excluded
+    }
 }
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
--- src/main/kotlin/com/github/jpvos/keylogger/plugin/services/KeyloggerService.kt
+++ src/main/kotlin/com/github/jpvos/keylogger/plugin/services/KeyloggerService.kt
@@ -3,6 +3,8 @@ package com.github.jpvos.keylogger.plugin.services
 import com.github.jpvos.keylogger.core.Action
 import com.github.jpvos.keylogger.core.Counter
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.diagnostic.thisLogger
+import java.sql.*
 
 
 @Service
@@ -17,6 +19,38 @@ class KeyloggerService : Counter.Listener {
             actions = emptyMap()
         )
         counter.setState(initialState)
+
+
+        Class.forName("org.sqlite.JDBC")
+        var connection: Connection? = null
+        try {
+            // create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite:main.db")
+            val statement: Statement = connection.createStatement()
+            statement.setQueryTimeout(30) // set timeout to 30 sec.
+
+            statement.executeUpdate("drop table if exists person")
+            statement.executeUpdate("create table person (id integer, name string)")
+            statement.executeUpdate("insert into person values(1, 'leo')")
+            statement.executeUpdate("insert into person values(2, 'yui')")
+            val rs: ResultSet = statement.executeQuery("select * from person")
+            while (rs.next()) {
+                // read the result set
+                thisLogger().warn("name = " + rs.getString("name"))
+                thisLogger().warn("id = " + rs.getInt("id"))
+            }
+        } catch (e: SQLException) {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.thisLogger().warn(e.message)
+        } finally {
+            try {
+                if (connection != null) connection.close()
+            } catch (e: SQLException) {
+                // connection close failed.
+                System.err.thisLogger().warn(e.message)
+            }
+        }
     }
 
     override fun onAction(action: Action) {
